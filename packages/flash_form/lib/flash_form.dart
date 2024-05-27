dfabce45681e3a4771dde25892554c4f7aa5dfc3
soraef <sorasorasorayama@gmail.com>
2024-05-27 12:33:59 +0900

    feat: Add FlashLayout and FieldWidgets to FlashForm package

--- packages/flash_form/lib/flash_form.dart
+++ packages/flash_form/lib/flash_form.dart
@@ -8,3 +8,5 @@ export 'src/flash_field_widget.dart';
 export 'src/flash_field_preview.dart';
 export 'src/flash_field_validator.dart';
 export 'src/flash_field_wrapper.dart';
+export 'src/flash_layout.dart';
+export 'src/field_widgets/field_widgets.dart';
---
+++ packages/flash_form/lib/src/field_widgets/field_widgets.dart
@@ -0,0 +1,2 @@
+export 'src/flash_dropdown_field.dart';
+export 'src/flash_text_field.dart';
