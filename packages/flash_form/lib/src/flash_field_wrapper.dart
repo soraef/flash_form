e3d12f17c52487b3d3e71df6dd4ee7544eb339c9
soraef <sorasorasorayama@gmail.com>
2024-05-28 15:23:15 +0900

    refactor: Update FlashForm package by optimizing code and removing unused imports

--- packages/flash_form/lib/src/flash_field_wrapper.dart
+++ packages/flash_form/lib/src/flash_field_wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:flash_form/flash_form.dart';
 import 'package:flutter/material.dart';
 
 import 'flash_field.dart';
@@ -5,29 +6,43 @@ import 'flash_field_validator.dart';
 
 class LableWrapper<TValue, TView> implements FieldWrapper<TValue, TView> {
   final String text;
+  final String? description;
+  final EdgeInsets padding;
 
-  LableWrapper(this.text);
+  LableWrapper(
+    this.text, {
+    this.description,
+    this.padding = const EdgeInsets.only(top: 16.0),
+  });
 
   @override
   Widget build(Widget fieldWidget, FlashField flashField) {
     final isRequired =
         flashField.validators.any((element) => element is RequiredValidator);
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Text(text, style: const TextStyle(fontSize: 12)),
-            if (isRequired)
-              const Text(
-                ' *',
-                style: TextStyle(color: Colors.red, fontSize: 12),
-              ),
-          ],
-        ),
-        fieldWidget,
-      ],
+    return Padding(
+      padding: padding,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(text, style: const TextStyle(fontSize: 12)),
+              if (isRequired)
+                const Text(
+                  ' *',
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
+            ],
+          ),
+          if (description != null)
+            Text(
+              description!,
+              style: const TextStyle(fontSize: 11, color: Colors.grey),
+            ),
+          fieldWidget,
+        ],
+      ),
     );
   }
 }
@@ -97,19 +112,32 @@ class DefaultValueWrapper<TValue, TView>
     implements FieldWrapper<TValue, TView> {
   final EdgeInsetsGeometry padding;
   final String? label;
+  final String? description;
+  final bool enableListRemoveButton;
 
   const DefaultValueWrapper({
     this.label,
-    this.padding = const EdgeInsets.symmetric(vertical: 4.0),
+    this.description,
+    this.padding = const EdgeInsets.only(bottom: 0.0),
+    this.enableListRemoveButton = true,
   });
 
   @override
   Widget build(Widget fieldWidget, FlashField flashField) {
+    final isListItem = flashField.isListItem;
     var widget = fieldWidget;
     if (label != null) {
-      widget = LableWrapper<TValue, TView>(label!).build(widget, flashField);
+      widget = LableWrapper<TValue, TView>(
+        label!,
+        description: description,
+      ).build(widget, flashField);
     }
     widget = ErrorMessageWrapper<TValue, TView>().build(widget, flashField);
+
+    if (isListItem && enableListRemoveButton) {
+      widget = const ListItemRemoveWrapper().build(widget, flashField);
+    }
+
     widget = PaddingWrapper(padding).build(widget, flashField);
     return widget;
   }
@@ -118,9 +146,11 @@ class DefaultValueWrapper<TValue, TView>
 class DefaultTypeWrapper<TValue, TView> implements FieldWrapper<TValue, TView> {
   final EdgeInsetsGeometry padding;
   final String? label;
+  final String? description;
 
   const DefaultTypeWrapper({
     this.label,
+    this.description,
     this.padding = const EdgeInsets.all(16.0),
   });
 
@@ -129,7 +159,10 @@ class DefaultTypeWrapper<TValue, TView> implements FieldWrapper<TValue, TView> {
     var widget =
         ErrorMessageWrapper<TValue, TView>().build(fieldWidget, flashField);
     if (label != null) {
-      widget = LableWrapper<TValue, TView>(label!).build(widget, flashField);
+      widget = LableWrapper<TValue, TView>(
+        label!,
+        description: description,
+      ).build(widget, flashField);
     }
     widget = CardWrapper(padding: padding).build(widget, flashField);
     return widget;
@@ -139,19 +172,80 @@ class DefaultTypeWrapper<TValue, TView> implements FieldWrapper<TValue, TView> {
 class DefaultListWrapper implements FieldWrapper {
   final EdgeInsetsGeometry padding;
   final String? label;
+  final String? description;
 
   const DefaultListWrapper({
     this.label,
-    this.padding = const EdgeInsets.all(0.0),
+    this.description,
+    this.padding = const EdgeInsets.only(bottom: 0.0),
   });
 
   @override
   Widget build(Widget fieldWidget, FlashField flashField) {
     var widget = ErrorMessageWrapper().build(fieldWidget, flashField);
     if (label != null) {
-      widget = LableWrapper(label!).build(widget, flashField);
+      widget = LableWrapper(
+        label!,
+        description: description,
+      ).build(widget, flashField);
     }
     widget = PaddingWrapper(padding).build(widget, flashField);
+    widget = const ListItemAddWrapper().build(widget, flashField);
     return widget;
+  }
+}
+
+class ListItemRemoveWrapper implements FieldWrapper {
+  final EdgeInsetsGeometry padding;
+
+  const ListItemRemoveWrapper({
+    this.padding = const EdgeInsets.all(8.0),
+  });
+
+  @override
+  Widget build(Widget fieldWidget, FlashField flashField) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(child: fieldWidget),
+        IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            flashField.sendEvent(ListItemRemoveEvent(flashField));
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ListItemAddWrapper implements FieldWrapper {
+  final EdgeInsetsGeometry padding;
+  final String? addText;
+
+  const ListItemAddWrapper({
+    this.padding = const EdgeInsets.only(top: 16.0),
+    this.addText = 'Add',
+  });
+
+  @override
+  Widget build(Widget fieldWidget, FlashField flashField) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        fieldWidget,
+        Padding(
+          padding: padding,
+          child: OutlinedButton(
+            child: Text(addText ?? 'Add'),
+            onPressed: () {
+              if (flashField is ListField) {
+                flashField.addField();
+              }
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
